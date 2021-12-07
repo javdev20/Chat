bf598c65b477dda70e2118fecb74d309cdf11834
javdev20 <islam.qayumov07@gmail.com>
2021-12-07 20:05:44 +0300

    implemented network interaction in client side

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,13 @@
+
 import examples.SimpleChatClient;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -37,6 +42,12 @@ public class Client {
 
         JButton sendButton = new JButton("Send");
 
+        sendButton.addActionListener(new SendButtonListener());
+        setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
         panel.add(qScroller);
         panel.add(outgoing);
         panel.add(sendButton);
@@ -44,8 +55,49 @@ public class Client {
         frame.getContentPane().add(BorderLayout.CENTER, panel);
         frame.setSize(650, 500);
         frame.setVisible(true);
+    }
 
+    private void setUpNetworking() {
+        try {
+            socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(socket.getOutputStream());
+            System.out.println("networking established");
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
 
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+
+    class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("client read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
     }
 }
